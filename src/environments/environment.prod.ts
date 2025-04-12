ad64898c7ab95ca861a51539aa24a05a7f197a42
Joshua Enad <152776204+XeneXD@users.noreply.github.com>
2025-04-13 07:36:33 +0800

    WeatherApp
    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiKey: '913ce52b74974b19de83e21080442515',
+  apiKey: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: false,
-  apiKey: '913ce52b74974b19de83e21080442515',
+  apiKey: '',
 };
